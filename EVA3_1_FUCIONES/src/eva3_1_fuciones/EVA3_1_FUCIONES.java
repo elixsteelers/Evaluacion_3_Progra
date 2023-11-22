0e5d48166f6631c39639f2f1581a5aafaab56109
elixsteelers <143569020+elixsteelers@users.noreply.github.com>
2023-11-21 22:10:26 -0600

    Add files via upload

---
+++ EVA3_1_FUCIONES/src/eva3_1_fuciones/EVA3_1_FUCIONES.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_1_fuciones;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_1_FUCIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //LLAMADA A FUNCION
+        imprimirMensaje("HOLA MUNDO!!!");
+    }
+  public static void imprimirMensaje(String mensaje){   
+      System.out.println(mensaje);
+  }
+}
